2b3e3855dca96479ecaefb7800b41afe472fbf8e
PercyNeo <nukeripercy@yahoo.com>
2022-09-09 20:40:31 +0200

    Task 2)

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main a program that prints the alphabet in lowercase using putchar
+  *return 0
+*/
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putcher(ch);
+	}
+	putcher('\n');
+	return (0);
+}
