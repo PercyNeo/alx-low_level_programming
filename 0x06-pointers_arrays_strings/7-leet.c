b2e0d832b04ffbdf8ef84e8a99aa5e569bbf2ada
PercyNeo <nukeripercy@yahoo.com>
2022-09-21 22:42:56 +0200

    O

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string into rot13
+ * leet - encodes a string into 1337
  * @s: string to encode
  *
  * Return: address of s
  */
-char *rot13(char *s)
+char *leet(char *s)
 {
 	int i, j;
-	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
 	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			if (a[j] == *(s + i))
-			{
 				*(s + i) = b[j];
-				break;
-			}
 		}
 	}
 	return (s);
