2989ebba5fce019997b939818c26d6687bda6ec1
PercyNeo <nukeripercy@yahoo.com>
2022-09-27 10:46:50 +0200

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
-* _memset - Fills the first n bytes of the memory area
+*_memset - fills a momery block with a constant byte
 *@s: address to memory block
-* @s: A pointer to the memory area to be filled.
-* @n: The number of bytes to be filled b.
-* @b: char to be used
-* * Return: A pointer to the filled memory area @s.
+*@b: char to be used
+*@n: number of bttes to be used
+*
+*Return: pointer to memory block
 */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 while (n)
 {
-	s[n - 1] = b;
-	n--;
+s[n - 1] = b;
+n--;
 }
 return (s);
 }
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
-unction that fills memory with a constant byte.
+1. function that fills memory with a constant byte
+2.  function that copies memory area.
